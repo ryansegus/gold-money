dbf12e9c2ac37423d44ca7ad482e9cda8d89f615
RyanGus <ryansegus@gmail.com>
2019-08-18 22:06:56 +0200

    initialDev: Add link to header logo.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,18 @@
+import Link from 'next/link';
 import logo from './../assets/img/coin-cap-logo.jpg';
-const headerStyle = {
+const headerLogoStyle = {
   display: 'block',
   width: '100%',
   maxWidth: 300,
-  margin: '0 auto'
+  margin: '0 auto',
+  cursor: 'pointer'
 };
 
 const Header = () => (
   <header>
-    <img src={logo} alt="CoinCap Logo" style={headerStyle} />
+    <Link href="/">
+    <img src={logo} alt="CoinCap Logo" style={headerLogoStyle} />
+    </Link>
   </header>
 );
 
